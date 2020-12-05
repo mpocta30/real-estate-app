41faf119a72bd2651142efe456e0bafb6acf1a25
mpocta30 <mpocta30@vt.edu>
2020-12-05 16:47:22 -0500

    Added some firebase configs

---
+++ server/config/config.js
@@ -0,0 +1,18 @@
+databaseURL = '****'
+serviceAccount = {
+    "type": "service_account",
+    "project_id": "keebaux",
+    "private_key_id": "d11eed5488f150227c8c7dd1adeb1846544a0951",
+    "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvwIBADANBgkqhkiG9w0BAQEFAASCBKkwggSlAgEAAoIBAQD5R/WVDbXkA6gm\nlJKF21jLT6vI9pUsGLZOGxcLyBpfoIVQGLNGAaqJmP9GxsjQVNUJTT4mB4Y3xi3u\nwR3dip1CecXxVICIhMgSuoi8NjIjvZMLgrBFOBSNWG1ncaBB6/zlFpcgfPTNNJTu\nIfAVP06rUJPfvOnwejMYdfr4pXpPsTI9pVBhnQd++XFHawjlFxSCHf4EKTnAIUQu\npOpYPoVgHqg2+Z0OvaR6eNDafDCzysuX9nbk2l9EbdvNrjqpuAoYDbMvFivnBwmd\nWTbsGSXlfafEg1spS0E7XlF4d7SukL2ZfSb4pBZ6TqiHVEAcOPGSGXttsnB4C9cq\nV5x4vV/1AgMBAAECggEAMSibqXU2tWxzMIiEDJmg/RcNDWhzKmsFKIn3FX+0k78Y\nCx+T2u5hLxlY3iA9YbgY+uicpOs+8KjIThkPRq0bLgzNmDXN0x48RlU6ucMGSD4O\nTWrMUBorE2Sozol9zQI4sQSvcNyzefQwmnVe3xHI9cfCIZEjoSS4Bs7dW0Nmx2gK\nmYiuHKlyrJRnm9jFwg/n2nTitOMedHs3MSnOqijjUoPyXJkBYrHIYaHRwY6OKfYG\nur304tG+vnKAaRIuTlhy7ZQ+dpTnNAP889xM00z6IIRryG+CCsugz7BKt0CDbKRw\nEW0AtAIXhhTTeBS2u5eH8zkfN/sP0xGEKRzQJuthgQKBgQD9wGFJjVDHIzrW5hfu\n3qwAab5nIAlRROWcCadjYU4ePIWrDPJgb0x5YzPpcDtD5NAFu0YNtMhTL6UQMZ2u\nJ0EzVsj3hikUDYRn3CIy2UUJkmDzCv1k6TKqzJGq+IjKAYeehORJfw5spePJGYLi\neaMwJfqZvqI2UttE9vEuT/GevQKBgQD7fXA+0e36WyEBiSvFRPqcFyuXA7PE1a7y\nTG2k/o3jOpbpWGRLrtRiuUGCU6qMInyfBeBal/ipbtjSq3axGsYLzO6bRu2cyJIQ\nIOci0tou6ihtKS0DmFPwxXU1mkDy0GBDVP3sbgphzdWRckBQjkGy8o6NCrCtOlF3\nw6nAuZAVmQKBgQCHYF+G8hGqbC00AUW7dqik/8VJF1phNTSabrwS1RLsJrXZb+rP\ncBGIoVhMrkewDR6+MA9LuKs3Vi/l19Kb2u16Ck7lX3oC7MM69cJ5BllCpgl7IN3X\nPGJ/pOwCHt47h11nQNbaLyLu7AH+g9HEKcjaIyjzZTT9bm70+Bnr71KksQKBgQDq\nr6ZdmT5BUWU9r6o/Dx3nJ+Avru7FkrsNtbDkNgBmujg2TZxa3C2qa0LswfL3jv9I\nzw0MvaD5TE3eWtAKefey720DTm9SzgEwN+Q6JRYijbVoGPK+1hoPJ9U3Tlmpvjb2\n3ouPZSkfGBxi/IF0Kyy4N447pGw3W6LG5rq09PW2kQKBgQDxqNCJ0QHbIUnmaetF\njBPVJNEK92YvUPCiA/lF32lVhROfFx2JwjUh+obycAMoNEL29se1hqxkoPAaLLbR\n/D942OhQOtPchPUsTv5wXe5xIRZH/WXldNpc93zmAVV52DlbSd3GqS+kyfi540S6\nr4Rp63TkRcXglSe1YIF1fJ1Y/A==\n-----END PRIVATE KEY-----\n",
+    "client_email": "firebase-adminsdk-dxssu@keebaux.iam.gserviceaccount.com",
+    "client_id": "103271945245009547933",
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-dxssu%40keebaux.iam.gserviceaccount.com"
+  }
+  
+exports.databaseURL = databaseURL;
+exports.serviceAccount = serviceAccount;
+
+  
